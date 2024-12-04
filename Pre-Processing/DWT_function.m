049a15293d8afb15a3118ab9e4fcbbc2161517eb
TechAvi-eng <n.dhulashia@gmail.com>
2024-12-04 12:44:22 +0000

    Added function to calculate the DWT at desired level and output the coefficients. The input of this can be used to feed into the CNN, as 4 channels. This function optionally can display the coefficients for visualisation purposes only. It is ready to use.

--- Pre-Processing/DWT_function.m
+++ Pre-Processing/DWT_function.m
@@ -35,9 +35,6 @@ function reconstructed_img = denoise(img)
     % S = bookkeeping matrix
     [C, S] = wavedec2(img, n, wavelet);
     
-    % Get detail coefficients for the current level
-    [H, V, D] = detcoef2('all', C, S, n);
-    
     % Get approximation coefficients for the current level
     A = appcoef2(C, S, wavelet, n);
     
@@ -46,4 +43,55 @@ function reconstructed_img = denoise(img)
 end
 
 
-reconstructed_image = denoise(img); % Apply the function
+function [approx_coeff, vert_coeff, horiz_coeff, diag_coeff] = coefficients(input_image)
+    % DWT_coefficients
+    % This function takes an input image, performs the DWT at a specified
+    % decomposition level, and returns the approximation, vertical,
+    % horizontal, and diagonal coefficients.
+    
+    % Input arguments:
+    % input_image: the reconstructed image
+    
+    % Outputs:
+    % approx_coeff: Approximation coefficients
+    % vert_coeff: Vertical detail coefficients
+    % horiz_coeff: Horizontal detail coefficients
+    % diag_coeff: Diagonal detail coefficients
+    
+    % Optionally: Display a plot containing the coefficients
+
+    % Initialisation of parameters
+    wavelet = 'db5'; % Wavelet function
+    n = 2; % Levels of decomposition
+    
+    % Perform wavelet decomposition up to the specified level
+    [C, S] = wavedec2(input_image, n, wavelet);
+    
+    % Extract approximation coefficients
+    approx_coeff = appcoef2(C, S, wavelet, n);
+    
+    % Extract detail coefficients: vertical, horizontal, and diagonal
+    [vert_coeff, horiz_coeff, diag_coeff] = detcoef2('all', C, S, n);
+    
+    % OPTIONAL: Display the coefficients for the current level
+    figure
+    subplot(2, 2, 1)
+    imagesc(approx_coeff)
+    colormap(gray)
+    title(['Approximation Coef. of Level ' num2str(n)])
+
+    subplot(2, 2, 2)
+    imagesc(horiz_coeff)
+    title(['Horizontal Detail Coef. of Level ' num2str(n)])
+
+    subplot(2, 2, 3)
+    imagesc(vert_coeff)
+    title(['Vertical Detail Coef. of Level ' num2str(n)])
+
+    subplot(2, 2, 4)
+    imagesc(diag_coeff)
+    title(['Diagonal Detail Coef. of Level ' num2str(n)])
+end
+
+reconstructed_image = denoise(img); % Apply denoising function 
+[approx_coeff, vert_coeff, horiz_coeff, diag_coeff] = coefficients(reconstructed_image); % Calculate the DWT coefficients (For CNN input)
