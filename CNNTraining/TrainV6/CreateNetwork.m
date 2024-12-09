8dab58de577ab8fbcb7960243ca45876cc78f813
Yigit <yigit.yigit.dogan@gmail.com>
2024-12-10 02:30:36 +0300

    More custimization functionality to training and evaluation

--- CNNTraining/TrainV6/CreateNetwork.m
+++ CNNTraining/TrainV6/CreateNetwork.m
@@ -29,36 +29,43 @@ params = createMaskRCNNConfig(imageSize, numClasses, classNames);
 
 
 
-    params.RPNROIPerImage = params.NumRegionsToSample;
-    params.MinSize=[3 3] %stands to reason that any smaller box than 3x3 pixels does not contain enough information to segment, can filter to save on computation
-    %could probably be increased even further, but computation is not a limiting factor here
-
-     params.AnchorBoxes=[
-     32    16
-    64    32
-   128    64
-   256   128
-   512   256
-    32    32
-    64    64
-   128   128
-   256   256
-   512   512
-    16    32
-    32    64
-    64   128
-   128   256
-   256   512
-   8 8
-   16 8
-   8 16] %anchor boxes set to reflect cell sizes in cell images, weighted for smaller values
-
-    params.NumAnchors=size(params.AnchorBoxes,1) %number of anchor boxes is the number of rows
+   %  params.RPNROIPerImage = params.NumRegionsToSample;
+   %  params.MinSize=[3 3] %stands to reason that any smaller box than 3x3 pixels does not contain enough information to segment, can filter to save on computation
+   %  %could probably be increased even further, but computation is not a limiting factor here
+   % 
+   %   params.AnchorBoxes=[
+   %   32    16
+   %  64    32
+   % 128    64
+   % 256   128
+   % 512   256
+   %  32    32
+   %  64    64
+   % 128   128
+   % 256   256
+   % 512   512
+   %  16    32
+   %  32    64
+   %  64   128
+   % 128   256
+   % 256   512
+   % 8 8
+   % 16 8
+   % 8 16] %anchor boxes set to reflect cell sizes in cell images, weighted for smaller values
+   % 
+   %  params.NumAnchors=size(params.AnchorBoxes,1) %number of anchor boxes is the number of rows
 
 disp(params);
+
+params.NumStrongestRegionsBeforeProposalNMS
 
 
 dlnet = createMaskRCNN(numClasses, params, 'resnet101'); %or resnet 101
 %%
-save("NNET101.mat", "dlnet", 'params')
+
+params.ImageSize=[520 704 1]
+
+
+%%
+save("1ChannelNet.mat", "dlnet", 'params')
 
