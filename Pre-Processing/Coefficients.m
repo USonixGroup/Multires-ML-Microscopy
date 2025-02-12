a71a5176ccac7958849fa514461fa03e9ee733bc
TechAvi-eng <n.dhulashia@gmail.com>
2025-02-12 18:01:38 +0000

    Extract 'Coefficient' thresholding function into separate file. Improved argument passing into function allowing optional parameters.

---
+++ Pre-Processing/Coefficients.m
@@ -0,0 +1,94 @@
+function thresh_reconstructed_img = Coefficients(img, wavelet, n, threshold_in)
+    % OPTIONAL arguments:
+    % Positional arguments means arguments position must be filled in
+    % function call to identify arguments that come after it
+
+    % Default argument values specified
+    arguments
+        img
+        wavelet = 'db5' % default value
+        n = 4 % default value
+        threshold_in = 0.1; % default value
+    end
+
+    % If input is a scalar value then create an array
+    if isscalar(threshold_in)
+        threshold = ones(1, n) * threshold_in;
+    else
+        threshold = threshold_in;
+    end
+    
+    
+    % If input is a scalar value then create an array
+    if isscalar(threshold_in)
+        threshold = ones(1, n) * threshold_in;
+    else
+        % If input is already an array
+        threshold = threshold_in;
+    end
+
+    %% Discrete Wavelet Transform    
+    % Loops through each decomposition level calculating coefficients from
+    % previous level approximation coefficients
+    for level = 1:n
+        if level == 1
+            [cA, cH, cV, cD] = dwt2(img, wavelet);
+        else
+            [cA, cH, cV, cD] = dwt2(cA, wavelet);
+        end
+        
+        % Dynamic threshold which can be different for each decomposition level 
+        t = threshold(level);
+
+        % Calculating thresholding parameters
+        cH_max = t * max(abs([cH(:)]));
+        cV_max = t * max(abs([cV(:)]));
+        cD_max = t * max(abs([cD(:)]));
+
+        coeffs{level, 1} = cA;
+        coeffs{level, 2} = wthresh(cH,'s',cH_max);
+        coeffs{level, 3} = wthresh(cV,'s',cV_max);
+        coeffs{level, 4} = wthresh(cD,'s',cD_max);
+
+    end
+    
+    %% Reconstruction
+    % Obtaining highest decomposition level approximation coefficients
+    cA_rec = coeffs{n, 1};
+
+    % Loops through each decomposition level (highest level of 
+    % decomposition to lowest) and calculates the reconstructed
+    % approximation coefficients for the next level
+    for level = n:-1:2
+        cH = coeffs{level, 2};
+        cV = coeffs{level, 3};
+        cD = coeffs{level, 4};
+        cA_rec = idwt2(cA_rec, cH, cV, cD, wavelet);
+        
+        % Bug fix - resolving issue with extra column
+        if size(cA_rec,2) == 96
+            cA_rec(:,96) = [];
+        end
+        
+        coeffs{level-1, 1} = cA_rec;
+    end
+
+    % Reconstruct the image using the thresholded coefficients (level 1
+    % decomposition coefficients)
+    cA = coeffs{1, 1};
+    cH = coeffs{1, 2};
+    cV = coeffs{1, 3};
+    cD = coeffs{1, 4};
+    thresh_reconstructed_img = idwt2(cA, cH, cV, cD, wavelet);
+
+    % Display the original and reconstructed images
+    figure;
+    subplot(1,2,1);
+    imshow(img, []);
+    title('Original Image');
+
+    subplot(1,2,2);
+    imshow(thresh_reconstructed_img, []);
+    title('Thresholded Reconstructed Image');
+    
+end
--- Pre-Processing/DWT_Thresholding.m
+++ Pre-Processing/DWT_Thresholding.m
@@ -2,89 +2,6 @@ clear all
 close all
 clc
 
-function thresh_reconstructed_img = coefficients(img)
-
-    % Initialise Parameters
-    wavelet = 'db5';
-    n = 4; % Bug means current max n = 6
-
-    threshold_in = [0.1 0.2 0.3 0.4]; % Threshold (decimal). Alternatively, threshold_in could be a single scalar value.
-
-    % If input is a scalar value then create an array
-    if isscalar(threshold_in)
-        threshold = ones(1, n) * threshold_in;
-    else
-        % If input is already an array
-        threshold = threshold_in;
-    end
-
-    %% Discrete Wavelet Transform    
-    % Loops through each decomposition level calculating coefficients from
-    % previous level approximation coefficients
-    for level = 1:n
-        if level == 1
-            [cA, cH, cV, cD] = dwt2(img, wavelet);
-        else
-            [cA, cH, cV, cD] = dwt2(cA, wavelet);
-        end
-        
-        % Dynamic threshold which can be different for each decomposition level 
-        t = threshold(level);
-
-        % Calculating thresholding parameters
-        cH_max = t * max(abs([cH(:)]));
-        cV_max = t * max(abs([cV(:)]));
-        cD_max = t * max(abs([cD(:)]));
-
-        coeffs{level, 1} = cA;
-        coeffs{level, 2} = wthresh(cH,'s',cH_max);
-        coeffs{level, 3} = wthresh(cV,'s',cV_max);
-        coeffs{level, 4} = wthresh(cD,'s',cD_max);
-
-    end
-    
-    %% Reconstruction
-    % Obtaining highest decomposition level approximation coefficients
-    cA_rec = coeffs{n, 1};
-
-    % Loops through each decomposition level (highest level of 
-    % decomposition to lowest) and calculates the reconstructed
-    % approximation coefficients for the next level
-    for level = n:-1:2
-        cH = coeffs{level, 2};
-        cV = coeffs{level, 3};
-        cD = coeffs{level, 4};
-        cA_rec = idwt2(cA_rec, cH, cV, cD, wavelet);
-        
-        % Bug fix - resolving issue with extra column
-        if size(cA_rec,2) == 96
-            cA_rec(:,96) = [];
-        end
-        
-        coeffs{level-1, 1} = cA_rec;
-    end
-
-    % Reconstruct the image using the thresholded coefficients (level 1
-    % decomposition coefficients)
-    cA = coeffs{1, 1};
-    cH = coeffs{1, 2};
-    cV = coeffs{1, 3};
-    cD = coeffs{1, 4};
-    thresh_reconstructed_img = idwt2(cA, cH, cV, cD, wavelet);
-
-    % Display the original and reconstructed images
-    figure;
-    subplot(1,2,1);
-    imshow(img, []);
-    title('Original Image');
-
-    subplot(1,2,2);
-    imshow(thresh_reconstructed_img, []);
-    title('Thresholded Reconstructed Image');
-    
-end
-
-
 % MAKE SURE THE FOLDER CONTAINING IMAGES IS ADDED TO PATH AND YOU ARE IN
 % THE SAME FOLDER HIERACHY AS THE IMAGE FOLDER
 
@@ -105,5 +22,10 @@ im=imread(imagePath);
 img = mean(im,3);
 img = rescale(img);
 
+% Initialise Parameters
+wavelet = 'db5';
+n = 4; % Bug means current max n = 6
+threshold_in = [0.1 0.2 0.3 0.4]; % Threshold (decimal). Alternatively, threshold_in could be a single scalar value.
+
 % Applies the DWT thresholding function and returns the denoised image
-denoised_img = coefficients(img);
+denoised_img = Coefficients(img, wavelet, n, threshold_in);
