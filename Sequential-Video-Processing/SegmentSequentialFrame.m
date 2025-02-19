7f152fac55164ee11da83c770fb5d999cb5c338a
Yigit <yigit.yigit.dogan@gmail.com>
2025-02-19 22:10:42 +0000

    more on sequential processing

---
+++ Sequential-Video-Processing/SegmentSequentialFrame.m
@@ -0,0 +1,20 @@
+
+
+
+dlFeatures = predict(obj.FeatureExtractionNet, dlX, 'Acceleration','auto');
+
+[dlRPNScores, dlRPNReg] = predict(obj.RegionProposalNet, dlFeatures, 'Outputs',{'RPNClassOut', 'RPNRegOut'});
+
+% Call region proposal
+dlProposals = regionProposal(obj, dlRPNReg, dlRPNScores); %change to sequential RP using previous BBOXES
+
+dlPooled = roiAlignPooling(obj, dlFeatures, dlProposals, obj.PoolSize);
+
+dlFinalFeatures = predict(obj.PostPoolFeatureExtractionNet, dlPooled, dlPooled, 'Acceleration','auto');
+
+[dlBoxReg, dlBoxScores] = predict(obj.DetectionHeads, dlFinalFeatures, 'Acceleration','auto', 'Outputs',{'detectorRegOut', 'detectorClassOut'});
+
+outputFeatures{1} = dlProposals;
+outputFeatures{2}= dlBoxReg;
+outputFeatures{3}= dlBoxScores;
+outputFeatures{4}= dlFeatures;
