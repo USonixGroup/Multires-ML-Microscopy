307bcd2a2db7d7169ba1db0a8b4d4915baf4f1f8
TechAvi-eng <n.dhulashia@gmail.com>
2025-04-15 16:00:26 +0100

    Updating DWT_Testing.m script to integrate with DWT_Denoise.m function and display the before and after pre-processing images.

--- Pre-Processing/DWT_Testing.m
+++ Pre-Processing/DWT_Testing.m
@@ -19,6 +19,35 @@ im=imread(imagePath);
 img = mean(im,3);
 img = rescale(img);
 
-anasd = DWT_Denoise(img,"Threshold",0.4, "Level",2,"Wavelet",'db3');
+% Generate denoised images
+level_3 = DWT_Denoise(img, "Threshold", 0.1, "Level", 3, "Wavelet", 'db5');
+level_4 = DWT_Denoise(img, "Threshold", 0.1, "Level", 4, "Wavelet", 'db5');
+level_5 = DWT_Denoise(img, "Threshold", 0.1, "Level", 5, "Wavelet", 'db5');
 
-imshow(anasd)
+% Apply global settings for consistency
+set(groot, 'DefaultTextInterpreter', 'latex');
+set(groot, 'DefaultAxesTickLabelInterpreter', 'latex');
+set(groot, 'DefaultLegendInterpreter', 'latex');
+set(groot, 'DefaultAxesFontName', 'CMU Serif');
+set(groot, 'DefaultTextFontName', 'CMU Serif');
+
+% Create figure for subplots
+figure;
+t = tiledlayout(1,3,'Padding','None','TileSpacing','compact');
+
+% Level 3 image
+
+nexttile(1)
+imshow(level_3);
+title('Level 3 DWT Denoised', 'Interpreter', 'latex', 'FontName', 'CMU Serif');
+
+% Level 4 image
+nexttile(2)
+imshow(level_4);
+title('Level 4 DWT Denoised', 'Interpreter', 'latex', 'FontName', 'CMU Serif');
+
+% Level 5 image
+nexttile(3)
+imshow(level_5);
+title('Level 5 DWT Denoised', 'Interpreter', 'latex', 'FontName', 'CMU Serif');
+
